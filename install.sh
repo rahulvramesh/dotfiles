704f1888df3cb61f11dcb138b02757fa93578bd9
Rahul V Ramesh <rahulvramesh@example.com>
2024-11-29 07:40:12 +0000

    changes

--- install.sh
+++ install.sh
@@ -34,8 +34,25 @@ fi
 
 # Install Oh My Zsh if not already installed
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    echo "y" | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
+
+# Change default shell to zsh if not already
+if [ "$SHELL" != "/usr/bin/zsh" ]; then
+    chsh -s $(which zsh)
+fi
+
+# Node Version Manager installation and setup
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+
+# Add to ~/.zshrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# Install latest LTS version of Node.js
+nvm install --lts
+nvm use --lts
 
 # Source the new configurations
 source ~/.zshrc
